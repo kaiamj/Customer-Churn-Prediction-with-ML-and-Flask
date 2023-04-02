5c605f5d3337f7725d678df23ba7d0dced90a39c
kaiamj <kainaat562@yahoo.com>
2023-04-02 05:05:25 +0200

    eda

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys 
-import logging
+sys.path.append('.') # to let python know src module is in this directory 
+from src.logger import logging
+
 def error_msg_detail(error,error_detail:sys): # error details
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -15,9 +17,9 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__ == "__main__":
-    try:
-        check = 1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)
+# if __name__ == "__main__":
+#     try:
+#         check = 1/0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise CustomException(e,sys)
